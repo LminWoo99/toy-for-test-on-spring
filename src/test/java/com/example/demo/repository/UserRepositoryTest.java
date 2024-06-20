5f16504934238441653b213075e00818adb23ca8
LminWoo99 <mw310@naver.com>
2024-06-20 21:25:10 +0900

    불필요한 코드 제거 및 리네이밍

---
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -0,0 +1,61 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.UserStatus;
+import com.example.demo.service.UserService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+@Sql("/sql/user-repository-test-data.sql")
+class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+
+
+
+    @Test
+    void findByIdAndStatus_로_유저데이터를_찾아올_수_있다() throws Exception{
+
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.ACTIVE);
+
+        //then
+        assertThat(result.isPresent()).isTrue();
+
+    }
+    @Test
+    void findByIdAndStatus는_데이터가_없으면_Optional_empty_를_내려준다() throws Exception{
+
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.PENDING);
+
+        //then
+        assertThat(result.isPresent()).isFalse();
+        assertThat(result.isEmpty()).isTrue();
+
+    }
+    @Test
+    void findByEmailAndStatus_로_유저데이터를_찾아올_수_있다() throws Exception{
+
+        Optional<UserEntity> result = userRepository.findByEmailAndStatus("mw310@naver.com", UserStatus.ACTIVE);
+
+        //then
+        assertThat(result.isPresent()).isTrue();
+
+    }
+    @Test
+    void findByEmailAndStatus는_데이터가_없으면_Optional_empty_를_내려준다() throws Exception{
+
+        Optional<UserEntity> result = userRepository.findByEmailAndStatus("mw310@naver.com", UserStatus.PENDING);
+
+        //then
+        assertThat(result.isPresent()).isFalse();
+        assertThat(result.isEmpty()).isTrue();
+
+    }
+}
---
+++ src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,3 @@
+
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (1, 'mw310@naver.com', 'manu', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
