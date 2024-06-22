28278de697b7743d3920561916789f5f0e6de7eb
LminWoo99 <mw310@naver.com>
2024-06-22 23:05:07 +0900

    test: UserCreateControllerTest 테스트 코드 추가

--- src/test/java/com/example/demo/controller/UserCreateControllerTest.java
+++ src/test/java/com/example/demo/controller/UserCreateControllerTest.java
@@ -1,21 +1,65 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.dto.UserCreateDto;
+import com.example.demo.model.dto.UserUpdateDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.awt.desktop.AboutEvent;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
+@SqlGroup({
+        @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
 class UserCreateControllerTest {
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private JavaMailSender javaMailSender;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void 시용자는_회원가입을_할_수있고_회원가입된_사용자는_PENDING_상태이다() throws Exception{
+        //given
+        UserCreateDto userCreateDto = UserCreateDto.builder()
+                .email("mw310@naver.com")
+                .nickname("manu-4")
+                .address("Pangyo")
+                .build();
+        BDDMockito.doNothing().when(javaMailSender).send(any(SimpleMailMessage.class));
+        //when
+
+        //then
+        mockMvc.perform(post("/api/users").header("EMAIL", "mw310@naver.com")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userCreateDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.email").value("mw310@naver.com"))
+                .andExpect(jsonPath("$.nickname").value("manu-4"))
+                .andExpect(jsonPath("$.status").value("PENDING"));
+    }
 
 
 }
